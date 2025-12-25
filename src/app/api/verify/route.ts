db690cc33d25bd9059dd433f26d702e1de761f7c
bahhlahke <blakeja@gmail.com>
2025-12-25 16:49:37 -0500

    Update route.ts

--- src/app/api/verify/route.ts
+++ src/app/api/verify/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
 // Initialize Stripe without specifying apiVersion.
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {} as any);
 
 export async function GET(req: Request) {
   try {
